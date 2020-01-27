0b5596150ffdd50d0c4cf2588917b7b14159527a
gaby magnussen <gabymagnussen@gmail.com>
2020-01-27 17:30:01 -0600

    updated script, index, and style

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 var m = moment();
 console.log(m);
-var currentDay = document.getElementById('currentDay');
 
-$("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+
+// moment(displayDay).format('mm/dd/yyyy');
+
+// var displayMoment = document.getElementById('displayDay');
+
+
+
+
 
 $(document).ready(function(){
+$("#displayWeekday").html(moment().format( "dddd"));
+$("#displayDay").html(moment().format( "MMMM Do YYYY"));
 /////9am row///////////////////////////////////////
 var row = $(`<div data-time=9 id='9' class="row">`);
 var col1 = $('<div class="col-sm-1"> <p class="hour">' + '9am' + '</p>');//column 1 is the hour 
-var col2 = $(`<div class="col-sm-9 past"><textarea id=text9 class="eventarea"></textarea>`);   //column 2 is the text area
+var col2 = $(`<div class="past col-sm-9"><textarea id=text9 class="eventarea"></textarea>`);   //column 2 is the text area
 var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=9><i class="fas fa-save"></i></button>`)//column 3 is the save button
 row.append(col1);
 row.append(col2);
@@ -18,7 +27,7 @@ $(".container").append(row); //append row to container
 ///10am row//////////////////////
 var row = $(`<div data-time='10' id='10' class="row">`);
 var col1 = $('<div class="col-sm-1"> <p class="hour">' + '10am' + '</p>');
-var col2 = $('<div class="col-sm-9 past"><textarea id=text10 class="eventarea"></textarea>'); 
+var col2 = $('<div class="past col-sm-9"><textarea id=text10 class="eventarea"></textarea>'); 
 var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=10><i class="fas fa-save"></i></button>');
 row.append(col1);
 row.append(col2);
@@ -28,7 +37,7 @@ $(".container").append(row);
 // 11am row
 var row = $(`<div data-time='11' id='11' class="row">`);
 var col1 = $('<div class="col-sm-1"> <p class="hour">' + '11am' + '</p>');
-var col2 = $('<div class="col-sm-9 past"><textarea id=text11 class="eventarea"></textarea>'); 
+var col2 = $('<div class="past col-sm-9"><textarea id=text11 class="eventarea"></textarea>'); 
 var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=11><i class="fas fa-save"></i></button>');
 row.append(col1);
 row.append(col2);
@@ -37,9 +46,9 @@ $(".container").append(row);
 
 
 // 12pm row
-var row = $(`<div data-time='11' id='12' class="row">`);
+var row = $(`<div data-time='12' id='12' class="row">`);
 var col1 = $('<div class="col-sm-1"> <p class="hour">' + '12pm' + '</p>');
-var col2 = $('<div class="col-sm-9 past"><textarea id=text12 class="eventarea"></textarea>'); 
+var col2 = $('<div class="past col-sm-9 "><textarea id=text12 class="eventarea"></textarea>'); 
 var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=12><i class="fas fa-save"></i></button>');
 row.append(col1);
 row.append(col2);
@@ -47,11 +56,55 @@ row.append(col3);
 $(".container").append(row);
 
 
-var saveBtn = $('.saveBtn');
-saveBtn.on('click', function(){
-    let eventId = $(this).attr('id');
-    let eventText = $(this).parent().siblings().children('.description').val();
-    localStorage.setItem(eventId, eventText);
-    console.log(this);
+// 1pm row
+var row = $(`<div data-time='1' id='1' class="row">`);
+var col1 = $('<div class="col-sm-1"> <p class="hour">' + '1pm' + '</p>');
+var col2 = $('<div class="past col-sm-9"><textarea id=text1 class="eventarea"></textarea>'); 
+var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=1><i class="fas fa-save"></i></button>');
+row.append(col1);
+row.append(col2);
+row.append(col3);
+$(".container").append(row);
 
-});});
+// 2pm row
+var row = $(`<div data-time='2' id='2' class="row">`);
+var col1 = $('<div class="col-sm-1"> <p class="hour">' + '2pm' + '</p>');
+var col2 = $('<div class="past col-sm-9"><textarea id=text2 class="eventarea"></textarea>'); 
+var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=2><i class="fas fa-save"></i></button>');
+row.append(col1);
+row.append(col2);
+row.append(col3);
+$(".container").append(row);
+
+// 3pm row
+var row = $(`<div data-time='3' id='3' class="row">`);
+var col1 = $('<div class="col-sm-1"> <p class="hour">' + '3pm' + '</p>');
+var col2 = $('<div class="past col-sm-9"><textarea id=text3 class="eventarea"></textarea>'); 
+var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=3><i class="fas fa-save"></i></button>');
+row.append(col1);
+row.append(col2);
+row.append(col3);
+$(".container").append(row);
+
+// 4pm row
+var row = $(`<div data-time='4' id='4' class="row">`);
+var col1 = $('<div class="col-sm-1"> <p class="hour">' + '4pm' + '</p>');
+var col2 = $('<div class="past col-sm-9"><textarea id=text4 class="eventarea"></textarea>'); 
+var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=4><i class="fas fa-save"></i></button>');
+row.append(col1);
+row.append(col2);
+row.append(col3);
+$(".container").append(row);
+
+// 5pm row
+var row = $(`<div data-time='5' id='5' class="row">`);
+var col1 = $('<div class="col-sm-1"> <p class="hour">' + '5pm' + '</p>');
+var col2 = $('<div class="past col-sm-9"><textarea id=text5 class="eventarea"></textarea>'); 
+var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=5><i class="fas fa-save"></i></button>');
+row.append(col1);
+row.append(col2);
+row.append(col3);
+$(".container").append(row);
+
+
+});
