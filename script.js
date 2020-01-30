73095b6f1243b026f4baa6ec409f999fc6c0e7e7
gaby magnussen <gabymagnussen@gmail.com>
2020-01-29 21:59:49 -0600

    updated script

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var m = moment();
+var h = moment().hour()
+console.log('hour :', h);
 moment().hour()///tells you the hour that is currently
 console.log(m);
 var saveBtn = $('.saveBtn');
@@ -14,6 +16,12 @@ var row = $(`<div data-time=9 id='9' class="row">`);
 var col1 = $('<div class="col-sm-1"> <p class="hour">' + '9am' + '</p>');//column 1 is the hour 
 var col2 = $(`<div class="past col-sm-9"><textarea id=text9 class="eventarea"></textarea>`);   //column 2 is the text area
 var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=9><i class="fas fa-save"></i></button>`)//column 3 is the save button
+if (h == 9){
+    col2.addClass("present");
+}
+else if (h<9){
+    col2.addClass("future");
+}
 row.append(col1);
 row.append(col2);
 row.append(col3);//append all 3 columns to the row
@@ -53,6 +61,12 @@ var row = $(`<div data-time='10' id='10' class="row">`);
 var col1 = $('<div class="col-sm-1"> <p class="hour">' + '10am' + '</p>');
 var col2 = $('<div class="past col-sm-9"><textarea id=text10 class="eventarea"></textarea>'); 
 var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=10><i class="fas fa-save"></i></button>');
+if (h == 10){
+    col2.addClass("present");
+}
+else if (h<10){
+    col2.addClass("future");
+}
 row.append(col1);
 row.append(col2);
 row.append(col3);
@@ -75,6 +89,12 @@ var row = $(`<div data-time='11' id='11' class="row">`);
 var col1 = $('<div class="col-sm-1"> <p class="hour">' + '11am' + '</p>');
 var col2 = $('<div class="past col-sm-9"><textarea id=text11 class="eventarea"></textarea>'); 
 var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=11><i class="fas fa-save"></i></button>');
+if (h == 11){
+    col2.addClass("present");
+}
+else if (h<11){
+    col2.addClass("future");
+}
 row.append(col1);
 row.append(col2);
 row.append(col3);
@@ -97,6 +117,12 @@ var row = $(`<div data-time='12' id='12' class="row">`);
 var col1 = $('<div class="col-sm-1"> <p class="hour">' + '12pm' + '</p>');
 var col2 = $('<div class="past col-sm-9 "><textarea id=text12 class="eventarea"></textarea>'); 
 var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=12><i class="fas fa-save"></i></button>');
+if (h == 12){
+    col2.addClass("present");
+}
+else if (h<12){
+    col2.addClass("future");
+}
 row.append(col1);
 row.append(col2);
 row.append(col3);
@@ -119,6 +145,12 @@ var row = $(`<div data-time='1' id='1' class="row">`);
 var col1 = $('<div class="col-sm-1"> <p class="hour">' + '1pm' + '</p>');
 var col2 = $('<div class="past col-sm-9"><textarea id=text1 class="eventarea"></textarea>'); 
 var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=1><i class="fas fa-save"></i></button>');
+if (h == 13){
+    col2.addClass("present");
+}
+else if (h<13){
+    col2.addClass("future");
+}
 row.append(col1);
 row.append(col2);
 row.append(col3);
@@ -140,6 +172,12 @@ var row = $(`<div data-time='2' id='2' class="row">`);
 var col1 = $('<div class="col-sm-1"> <p class="hour">' + '2pm' + '</p>');
 var col2 = $('<div class="past col-sm-9"><textarea id=text2 class="eventarea"></textarea>'); 
 var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=2><i class="fas fa-save"></i></button>');
+if (h == 14){
+    col2.addClass("present");
+}
+else if (h<14){
+    col2.addClass("future");
+}
 row.append(col1);
 row.append(col2);
 row.append(col3);
@@ -161,6 +199,12 @@ var row = $(`<div data-time='3' id='3' class="row">`);
 var col1 = $('<div class="col-sm-1"> <p class="hour">' + '3pm' + '</p>');
 var col2 = $('<div class="past col-sm-9"><textarea id=text3 class="eventarea"></textarea>'); 
 var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=3><i class="fas fa-save"></i></button>');
+if (h == 15){
+    col2.addClass("present");
+}
+else if (h<15){
+    col2.addClass("future");
+}
 row.append(col1);
 row.append(col2);
 row.append(col3);
@@ -182,6 +226,12 @@ var row = $(`<div data-time='4' id='4' class="row">`);
 var col1 = $('<div class="col-sm-1"> <p class="hour">' + '4pm' + '</p>');
 var col2 = $('<div class="past col-sm-9"><textarea id=text4 class="eventarea"></textarea>'); 
 var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=4><i class="fas fa-save"></i></button>');
+if (h == 16){
+    col2.addClass("present");
+}
+else if (h<16){
+    col2.addClass("future");
+}
 row.append(col1);
 row.append(col2);
 row.append(col3);
@@ -203,6 +253,12 @@ var row = $(`<div data-time='5' id='5' class="row">`);
 var col1 = $('<div class="col-sm-1"> <p class="hour">' + '5pm' + '</p>');
 var col2 = $('<div class="past col-sm-9"><textarea id=text5 class="eventarea"></textarea>'); 
 var col3 = $('<div class="col-sm-2"><button class="saveBtn" id=5><i class="fas fa-save"></i></button>');
+if (h == 17){
+    col2.addClass("present");
+}
+else if (h<17){
+    col2.addClass("future");
+}
 row.append(col1);
 row.append(col2);
 row.append(col3);
